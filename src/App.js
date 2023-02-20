4429ed683a4bb2e9a3681a2187c46be623da507a
manojkumars313 <manojraj.mm313@gmail.com>
2023-02-20 15:09:29 +0530

    React Handson_2

--- src/App.js
+++ src/App.js
@@ -1,39 +1,11 @@
-import React, { useState } from "react";
-import "./Components/Component.css";
-import ClassComponents from "./Components/ClassComponents";
-import FunctionComponents from "./Components/FunctionComponents";
+import React from "react";
+import Form from "./Components/Form";
+import "./Components/Component-style.css";
 
 function App() {
-  // const [funcHidden, setFuncHidden] = useState(true);
-  // const handleClickFunction = () => setFunc((Func) => !Func);
-
-  // const [classCompo, setClass] = useState(true);
-  // const handleClickClass = () => setFunc((ClassCompo) => !ClassCompo);
-  const [funcHidden, setFuncHidden] = useState(true);
-  const handleClickFunction = () => setFuncHidden((current) => !current);
-
-  const [clsHidden, setClsHidden] = useState(true);
-  const handleClickClass = () => setClsHidden((current) => !current);
-
   return (
-    <div className="Container">
-      <h1 className="heading">Styling using Functional and Class Component</h1>
-
-      {/* Button */}
-      <div className="btn_Box">
-        <button className="btn btn_Func" onClick={handleClickFunction}>
-          To see styling in Functional Component
-        </button>
-
-        <button className="btn btn_class" onClick={handleClickClass}>
-          To see styling in Class Component
-        </button>
-      </div>
-
-      <div className="component-box">
-        {!funcHidden ? <FunctionComponents /> : null}
-        {!clsHidden ? <ClassComponents /> : null}
-      </div>
+    <div className="App">
+      <Form />
     </div>
   );
 }
